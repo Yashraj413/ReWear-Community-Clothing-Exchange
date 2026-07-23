3b03723a3915bc2e1be1fa175313519a52989b17
Yash Raj <yashraj41323@gmail.com>
2026-07-23 23:33:39 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -47,14 +47,11 @@ export const decodeOrderNum = (str) => {
 };
 
 export const formatDateString = (dateString) => {
-  // 주어진 날짜 문자열을 Date 객체로 변환
   const date = new Date(dateString);
 
-  // 연도, 월, 일 추출
   const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, "0"); // 월은 0부터 시작하므로 +1 필요
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
   const day = date.getDate().toString().padStart(2, "0");
 
-  // 원하는 형식으로 문자열 생성
   return `${year}.${month}.${day}`;
 };
